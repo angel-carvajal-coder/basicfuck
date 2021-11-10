b8aebc5a43bb92227dbdb7dd26b48dcc7c6b500a
angel-carvajal-coder <angelcaru10@gmail.com>
2021-11-10 14:42:02 +0100

    Refactor compilation into a separate module

--- compile.py
+++ compile.py
@@ -1,5 +1,6 @@
 from sys import argv
 from util import *
+from parser_ import parse
 from collections import namedtuple
 
 Variable = namedtuple('Variable', ['name', 'length'])
@@ -54,40 +55,5 @@ for variable in variables:
 references = dict(references)
 
 # Start compiling!
-code = ''.join(lines)
-statements = code.split(';')
-output = ''
-
-for statement in statements:
-    cellPointer = 0
-    statement = statement.strip()
-    
-    op = '+'
-    splitByPlusOrMinusEquals = stripAll(statement.split('+='))
-    if len(splitByPlusOrMinusEquals) <= 1:
-        op = '-'
-        splitByPlusOrMinusEquals = stripAll(statement.split('-='))
-
-    splitByArrow = stripAll(splitByPlusOrMinusEquals[0].split('->'))
-    if splitByArrow[0] in references.keys():
-        reference = references[splitByArrow[0]]
-        reference = reference + (int(splitByArrow[1]) if len(splitByArrow) > 1 else 0)
-        cellPointer += reference
-        output += '>' * reference
-
-    if len(splitByPlusOrMinusEquals) > 1:
-        try:
-            output += '+' * int(splitByPlusOrMinusEquals[1])
-        except ValueError:
-            reference2 = references[splitByPlusOrMinusEquals[1]]
-            diff = reference2 - reference
-            cellPointer += diff
-            output += '>' * diff + '[-' + '<' * diff + op + '>' * diff + ']'
-
-            #output += 
-    
-    output += '<' * cellPointer
-    output += '\n'
-
-
+output = parse(''.join(lines), references)
 print(end=output)
